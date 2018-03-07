c9047829be52dfd20cb812b610f77a060f1b977b
vandosant <sjskender@gmail.com>
2018-03-07 12:35:21 -0700

    Import web3 and display lottery owner

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import lottery from './lottery'
 
 class App extends Component {
+  state = {
+    manager: ''
+  }
+
+  async componentDidMount () {
+    const manager = await lottery.methods.manager().call()
+    this.setState({ manager })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Lottery Contract</h2>
+        <p>This contract is managed by: {this.state.manager}</p>
       </div>
     );
   }
